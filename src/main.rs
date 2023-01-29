b482b04e125e19078cb65e3c109b675c885012ec
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2023-01-29 02:26:36 -0500

    get user input through cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+
+/*
+const MIN_CELLS: u32 = 30_000; 
+const MIN_ADD_CELLS: u16 = 500;
+*/
+//let mem: Vec<u8> = vec![0u8; MIN_CELLS as usize];
+
 fn main() {
-    println!("Hello, world!");
+	// Get the CLI aruments the exe was called with.
+	let args: Vec<String> = env::args().collect();
+
+	if let Some(filepath) = args.get(1) {
+		// Do stuff with filepath
+		dbg!(&filepath);
+	} else {
+		eprintln!("Please provide a file to parse!");
+	}
 }
