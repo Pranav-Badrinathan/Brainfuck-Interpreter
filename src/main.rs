4aaf86dbb8c20a541db35a5c2d4c988b5edc50c1
Pranav Badrinathan <pranav.badrinathan@gmail.com>
2023-01-30 01:10:41 -0500

    Can read contents of file on filepath taken from cli arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs;
+use std::process;
 
 /*
 const MIN_CELLS: u32 = 30_000; 
@@ -9,11 +11,22 @@ const MIN_ADD_CELLS: u16 = 500;
 fn main() {
 	// Get the CLI aruments the exe was called with.
 	let args: Vec<String> = env::args().collect();
+	
+	let file_contents: String;
 
-	if let Some(filepath) = args.get(1) {
-		// Do stuff with filepath
-		dbg!(&filepath);
+	if let Some(file_path) = args.get(1) {	
+		if let Ok(file_result) = fs::read_to_string(&file_path) {
+		    file_contents = file_result;
+		} else {
+			eprintln!("File not found: Did you provide the correct path?");
+			process::exit(1);
+		}
+		
+		dbg!(&file_path);
 	} else {
 		eprintln!("Please provide a file to parse!");
+		process::exit(1);
 	}
+
+	println!("{file_contents}");
 }
